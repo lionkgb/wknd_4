d29a2d6c54618d4e77b613907a8b55fb37e49d87
lionkgb <lionceau.lk@gmail.com>
2017-02-06 08:31:04 +0200

    Missing letter method

--- algo/missing_letter.rb
+++ algo/missing_letter.rb
@@ -2,8 +2,18 @@
 # as a string. If there is no missing letter, the method should return nil.
 # bonus: returns a string of all missing letters as a string. ex: find_missing_letter("ace") would return "bd", write your own test.
 
-def find_missing_letter(range)
-
+def missing_letter(range)
+  given_letters = range.split("")
+  all_letters = (given_letters[0]..given_letters[-1]).to_a 
+    all_letters.each do |letter|
+      if given_letters.include?(letter)   
+      elsif not given_letters.include?(letter)
+        return letter
+      end
+      if given_letters == all_letters
+        return nil
+      end
+  end
 end
 
 # Driver code - don't touch anything below this line.
