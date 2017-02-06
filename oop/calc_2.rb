0a8bf7e2d462fda6dc685bd8e792abd73b1eedc5
lionkgb <lionceau.lk@gmail.com>
2017-02-06 08:54:19 +0200

    Ruby Mixin Module

--- oop/calc_2.rb
+++ oop/calc_2.rb
@@ -3,7 +3,7 @@
 # a different technique using Composition. Hint: Google 'Ruby Mixin Module'. (Using
 # mixin modules is how you achieve Composition with Ruby.)
 
-class SimpleCalculator
+module Calculator
 
   def add(first_number, second_number)
     first_number + second_number
@@ -25,27 +25,51 @@ end
 
 class FancyCalculator
 
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+  include Calculator
 
   def square_root(number)
     Math.sqrt(number)
   end
-
 end
 
 # Copy your driver code from the previous exercise below:
 
+puts "Testing fancy_calculator"
+puts
+
+fancy_calculator = FancyCalculator.new
+
+result = fancy_calculator.add(2,7)
+if result == 9
+  puts "PASS"
+else 
+  puts "F"
+end
+
+result = fancy_calculator.subtract(8,4)
+if result == 4
+  puts "PASS"
+else 
+  puts "F"
+end
+
+result = fancy_calculator.multiply(6,4)
+if result == 24
+  puts "PASS"
+else
+  puts "F"
+end
+
+result = fancy_calculator.divide(6,3)
+if result == 2
+  puts "PASS"
+else
+  puts "F"
+end
+
+result = fancy_calculator.square_root(25)
+if result == 5
+  puts "PASS"
+else
+  puts "F"
+end
